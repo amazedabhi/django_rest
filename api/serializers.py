11dd51fa1e90e0131aabab0975c918bfd9e4dede
justtabhi <abhi.in12@gmail.com>
2020-09-16 21:53:15 +0530

    Added tag serializer in order tp keep it separate from article

--- api/serializers.py
+++ api/serializers.py
@@ -7,10 +7,19 @@ class StudentSerializer(serializers.Serializer):
     roll_no = serializers.IntegerField()
     marks = serializers.IntegerField()
 
+#For tags we use another serializers
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Tag
+        fields = '__all__'
 
-##Model serializer provide a class Meta which saves us from creating the
+
+#Model serializer provide a class Meta which saves us from creating the
 #fields of models again in serializers
 class ArticleSerializer(serializers.ModelSerializer):
+    #here read only is added because while deserializing the object to json we want to
+    #keep our tag separate from the Article
+    tags = TagSerializer(many =True,read_only = True)
     class Meta:
         model = models.Article
         fields = '__all__' 
